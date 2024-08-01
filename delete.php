829f279adfa354e4d6ab97508c69e6cc45ca12ed
Aryan Kumar Dubey <96795631+Aryan-we@users.noreply.github.com>
2024-08-02 04:46:05 +0530

    Update delete.php

--- delete.php
+++ delete.php
@@ -4,10 +4,8 @@ if(isset($_GET['delete'])){// checking if data is set in url
     $delete=$_GET['delete'];
     echo $delete;
     $delete_query="delete from phpcrud where id='$delete'";
-    echo "<script>conform("Do you really want to delete this data");</script>"
     $result=mysqli_query($connect,$delete_query);
     if($result){
-        echo"<script>alert('Data Deleted Successfully');</script>";
         header("location:display.php");
     }
 }
